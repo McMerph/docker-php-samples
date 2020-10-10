982214740a32e6d5ea96a2bf061cc0a128786521
Aleksandr Sidorenko <mcmerph@gmail.com>
2020-10-10 13:13:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to run
+
+ - Ensure you have Docker installed
+ - Ensure you have nodejs and npm installed
+ - Run `npm start` in terminal
---
+++ src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Title</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        echo "Hello, World!";
+        ?>
+    </p>
+</body>
+
+</html>
